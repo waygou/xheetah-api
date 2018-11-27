a187f42b97333a5c82bedc368e282de31b87ca7a
Waygou <bruno.falcao@waygou.com>
2018-11-27 21:54:59 +0000

    Apply fixes from StyleCI

--- routes/api2.php
+++ routes/api2.php
@@ -1,81 +1,75 @@
 <?php
 
 Route::get('system/device/enroll', function () {
-
-    $json = ['data' =>
-                ['response' => [
-                    'status' => 'OK',
+    $json = ['data' => ['response' => [
+                    'status'  => 'OK',
                     'message' => '',
-                    'link' => ''
+                    'link'    => '',
                 ],
                 'payload' => [
                     'handshake-token' => '328YEGUDYU3CGCG',
-                    'company' => [
+                    'company'         => [
                         'name' => 'Isto é Expresso',
-                        'id' => 1
-                    ]
-                ]
-                ]
+                        'id'   => 1,
+                    ],
+                ],
+                ],
             ];
 
     return response()->json($json);
 });
 
 Route::get('courier/authentication/login', function () {
-
-    $json = ['data' =>
-                ['response' => [
-                    'status' => 'OK',
+    $json = ['data' => ['response' => [
+                    'status'  => 'OK',
                     'message' => '',
-                    'link' => ''
+                    'link'    => '',
                 ],
                 'payload' => [
                     'courier' => [
                         'name' => 'Joao Pedro',
-                        'id' => 5
-                    ]
-                ]
-                ]
+                        'id'   => 5,
+                    ],
+                ],
+                ],
             ];
 
     return response()->json($json);
 });
 
 Route::get('courier/deliveries/active', function () {
-
-    $json = ['data' =>
-                ['response' => [
-                    'status' => 'OK',
+    $json = ['data' => ['response' => [
+                    'status'  => 'OK',
                     'message' => '',
-                    'link' => ''
+                    'link'    => '',
                 ],
                 'payload' => [
                     'delivery' => [
                         'related' => [
-                            'id' => 11
+                            'id' => 11,
                         ],
                         'address' => [
-                            'origin' => ['address' => 'Rua Morais Soares',
+                            'origin' => ['address'     => 'Rua Morais Soares',
                                          'door_number' => '30 3o Esq',
                                          'postal_code' => '1170-257',
-                                         'locality' => 'Lisboa',
-                                         'city' => 'Lisboa',
-                                         'country' => 'Portugal',
-                                         'latitude' => 38.7316,
-                                         'Longitude' => -9.12741],
+                                         'locality'    => 'Lisboa',
+                                         'city'        => 'Lisboa',
+                                         'country'     => 'Portugal',
+                                         'latitude'    => 38.7316,
+                                         'Longitude'   => -9.12741, ],
 
                             'destination' => ['address' => 'Rua da Penha de Franca',
-                                         'door_number' => '138',
-                                         'postal_code' => '1170-028',
-                                         'locality' => 'Lisboa',
-                                         'city' => 'Lisboa',
-                                         'country' => 'Portugal',
-                                         'latitude' => 38.7264,
-                                         'Longitude' => -9.13023]
-                        ]
-                    ]
-                ]
-                ]
+                                         'door_number'  => '138',
+                                         'postal_code'  => '1170-028',
+                                         'locality'     => 'Lisboa',
+                                         'city'         => 'Lisboa',
+                                         'country'      => 'Portugal',
+                                         'latitude'     => 38.7264,
+                                         'Longitude'    => -9.13023, ],
+                        ],
+                    ],
+                ],
+                ],
             ];
 
     return response()->json($json);
